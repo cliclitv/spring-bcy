8b17bcf6d508e494f5b2cd7ccbb1f2d4b368d3c5
yisar <1533540012@qq.com>
2024-04-21 16:48:03 +0800

    项目初始化

---
+++ src/main/java/net/banciyuan/bcy/BcyApplication.java
@@ -0,0 +1,13 @@
+package net.banciyuan.bcy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BcyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BcyApplication.class, args);
+	}
+
+}
