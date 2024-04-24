47af8de03cc1a0a1c3037c47ae81ffb049d6bf2e
yisar <1533540012@qq.com>
2024-04-24 13:31:20 +0800

    增加一个脚本

---
+++ setup.js
@@ -0,0 +1,27 @@
+const fs = require('fs')
+
+
+const yml =(databaseurl, databaseuser, databasepwd)=> `
+# spring.application.name=bcy
+spring:
+    application:
+        name: 'bcy'
+    datasource:
+        url: ${databaseurl}
+        username: ${databaseuser}
+        password: ${databasepwd}
+        driver-class-name: 'com.mysql.cj.jdbc.Driver' 
+
+mybatis:
+    mapper-locations: classpath:/mapper/*.xml
+        
+
+server:
+    port: 8888
+`
+const databaseurl = process.env.databaseurl || 'jdbc:mysql://localhost:3306/bcy'
+const databaseuser = process.env.databaseuser || 'root'
+const databasepwd = process.env.databasepwd || 'phpts'
+
+
+fs.writeFileSync('./src/main/resources/application.yml', yml(databaseurl, databaseuser, databasepwd), 'utf8');
