3910c8e3fb50260aab005f1afce91bcd61312a63
yisar <1533540012@qq.com>
2024-04-26 00:41:53 +0800

    搞定

--- src/main/java/bcy/dao/Post.java
+++ src/main/java/bcy/dao/Post.java
@@ -9,10 +9,28 @@ public class Post {
     private String status;
     private String sort;
     private String tag;
-    private Long uid;
     private Long pid;
     private Date createTime;
     private Date updateTime;
+    private Long uid;
+    private String name;
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    private String email;
 
     public Long getId() {
         return id;
